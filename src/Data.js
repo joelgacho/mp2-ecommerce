d989116b8c855fac7869977511f676c297c4987c
joelgacho <joelgacho78@gmail.com>
2023-02-09 01:36:30 +0800

    initialized

---
+++ src/Data.js
@@ -0,0 +1,59 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Rolex Submariner",
+    price: 200,
+    desc: "Features a polished brushed Oystersteel stainless steel case and Oyster bracelet rotating bezel.",
+    img: "images/rolex-1.png",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Rolex Cosmograph Daytona",
+    price: 300,
+    desc: "This 40mm two-tone watch features an Oystersteel case.",
+    img: "images/rolex-2.png",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "Rolex Daytona Black",
+    price: 400,
+    desc: "This 40mm watch features a solid 18k white gold ceramic bezel.",
+    img: "images/rolex-3.png",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Rolex Everose Diamond",
+    price: 300,
+    desc: " combines 904L-grade Oystersteel with 18 karat rose gold.",
+    img: "images/rolex-4.png",
+  },
+  {
+    id: "thiecbawdjksadjk",
+    name: "Rolex Champagne Dial",
+    price: 250,
+    desc: "This luxury watch for ladies features a 31mm stainless steel case.",
+    img: "images/rolex-5.png",
+  },
+  {
+    id: "iuertrywebncdjksadjk",
+    name: "Rolex Submariner Black Dial",
+    price: 350,
+    desc: "polished with brushed 904L Oystersteel stainless steel case.",
+    img: "images/rolex-6.png",
+  },
+  {
+    id: "thierytbvcbvzdhadjk",
+    name: "Rolex 18k Yellow Gold",
+    price: 500,
+    desc: "luted bezel and is fitted with a black leather strap with a pattern.",
+    img: "images/rolex-7.png",
+  },
+  {
+    id: "trfoiwfcnbcawdjksadjk",
+    name: "Rolex Milgauss Watch",
+    price: 210,
+    desc: " Features a polished 904L stainless steel case with a screw-down crown & case stainless oyster bracelet.",
+    img: "images/rolex-8.png"
+  },
+ 
+];
